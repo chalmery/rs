5c3972abd6bd84c7e31da470378b2b0a8027c066
chalmery <chaochaoycc@gmail.com>
2022-09-17 10:20:01 +0800

    改善错误处理，只返回有用数据

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     //这个是能接受Unicode字符，如果是别的会抛异常
@@ -8,7 +9,11 @@ fn main() {
     //env::args_os()//返回OsString 这个能获取非Unicode字符
     // println!("{:?}",args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args)
+    .unwrap_or_else(|err|{
+        println!("解析参数错误：{}",err);
+        process::exit(1);
+    });
 
     let contents = fs::read_to_string(config.filename)
     .expect("无法读取文件");
@@ -22,11 +27,15 @@ struct Config{
 }
 
 impl Config {
-    fn new(args:&[String]) -> Config{
+    //静态生命周期，其生命周期能够存活于整个程序期间。所有的字符串字面值都拥有 'static 生命周期
+    fn new(args:&[String]) -> Result<Config,&'static str>{
+        if args.len() <3{
+            return  Err("没有输入内容");
+        }
         //牺牲内存换来少管理所有权
         let query  = args[1].clone();
         let filename  = args[2].clone(); 
-        Config { query,filename }
+        Ok(Config { query,filename })
     }
 }
 
