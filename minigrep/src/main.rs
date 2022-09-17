1b07f39d1977c8856584f96362589387626048fd
chalmery <chaochaoycc@gmail.com>
2022-09-17 14:04:21 +0800

    标准错误和标准输出

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -12,12 +12,12 @@ fn main() {
     let config = minigrep::Config::new(&args)
     //unwrap表示打开包装，
     .unwrap_or_else(|err|{
-        println!("解析参数错误：{}",err);
+        eprintln!("解析参数错误：{}",err);
         process::exit(1);
     });
     //如果返回的是Result的Err类型也就是Box<dyn Error>>做处理
     if let Err(e) = minigrep::run(config){
-         println!("应用错误：{}",e);
+         eprintln!("应用错误：{}",e);
          process::exit(1);
     }
 
